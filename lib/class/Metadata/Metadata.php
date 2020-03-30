cc147b660ff6a5c92ac697000c86c065e7ae4dbd
lachlan-00 <lachlan.00@gmail.com>
2020-03-30 13:32:36 +1000

    docs again

--- lib/class/Metadata/Metadata.php
+++ lib/class/Metadata/Metadata.php
@@ -103,6 +103,7 @@ trait Metadata
     /**
      * @param Model\MetadataField $field
      * @param $data
+     * @throws \ReflectionException
      */
     public function updateOrInsertMetadata(MetadataField $field, $data)
     {
@@ -141,6 +142,7 @@ trait Metadata
      * @param string $propertie
      * @param boolean $public
      * @return Model\MetadataField
+     * @throws \ReflectionException
      */
     public function getField($propertie, $public = true)
     {
