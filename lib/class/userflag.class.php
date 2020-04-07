f3001502ec9d82312c7d2df1a69c54e426c62be6
lachlan <lachlan.00@gmail.com>
2020-04-07 13:24:39 +1000

    Revert "cache booleans with userflag"
    This reverts commit e3695b1e2ad4c6c4b65159d3a9592ee55c3e7452.

--- lib/class/userflag.class.php
+++ lib/class/userflag.class.php
@@ -81,9 +81,9 @@ class Userflag extends database_object
 
         foreach ($ids as $objectid) {
             if (!isset($userflags[$objectid])) {
-                $userflag = false;
+                $userflag = 0;
             } else {
-                $userflag = $userflags[$objectid];
+                $userflag = (int) ($userflags[$objectid]);
             }
             parent::add_to_cache('userflag_' . $type . '_user' . $user_id, $objectid, array($userflag));
         }
@@ -130,7 +130,7 @@ class Userflag extends database_object
 
         $key = 'userflag_' . $this->type . '_user' . $user_id;
         if (parent::is_cached($key, $this->id)) {
-            return parent::get_from_cache($key, $this->id);
+            return (double) parent::get_from_cache($key, $this->id)[0];
         }
 
         $sql = "SELECT `id`, `date` FROM `user_flag` WHERE `user` = ? " .
